7a430a26519bf26529acda5f281f37ecf0f180c8
alperenn123 <alperenn123@gmail.com>
2020-10-29 02:33:00 +0300

    RestAPI implemented using vanilla NodeJs

---
+++ utils.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+function writeDataToFile(filename, content) {
+  fs.writeFileSync(filename, JSON.stringify(content), "utf-8", (error) => {
+    if (error) console.log(error);
+  });
+}
+
+function getPostData(req) {
+  return new Promise((resolve, reject) => {
+    try {
+      var body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      req.on("end", () => {
+        resolve(body);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+module.exports = {
+  writeDataToFile,
+  getPostData,
+};
